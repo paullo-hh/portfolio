7ebd22214e3569088254e35247201d7ab6133881
Paulo H. Carvalho <contacthenry96@gmail.com>
2025-02-22 01:31:32 -0300

    Ajustado o design do front. PS: falta a responsividade e adicionar algumas habilidades que faltam.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ window.addEventListener('scroll', function (){
     header.classList.toggle('scrolling', window.scrollY > 0)
 })
 
+function formatarCelular(celular) {
+    celular = celular.replace(/\D/g, ""); // Remove qualquer coisa que não seja número
+    celular = celular.replace(/^(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3"); // Formata o celular
+    return celular;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const celularInput = document.querySelector("input[name='fone']");
+
+    celularInput.addEventListener("input", function () {
+        this.value = formatarCelular(this.value);
+    });
+});
+
 // window.addEventListener('scroll', function (){
 //     let btntop = document.querySelector('#button-top')
 //     btntop.classList.toggle('scrolling-down', window.scrollY < 200)
